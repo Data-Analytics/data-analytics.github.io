e38bff15034d2afed3e92a41ac76aa3c78b118f1
unknown <sushanth.thangamani@INFWDO424.gvkbio.com>
2013-12-02 11:26:19 +0530

    cliped to gradient color

--- Cliped-Sparkline/cliped_sparkline.js
+++ Cliped-Sparkline/cliped_sparkline.js
@@ -45,18 +45,20 @@ function onRender() {
   x.domain([data[0].date, data[data.length - 1].date]);
   y.domain(d3.extent(data, function(d) { return d.temperature; }));
 
-  svg.append("clipPath")
-      .attr("id", "clip-above")
-    .append("rect")
-      .attr("width", width)
-      .attr("height", y(55));
-
-  svg.append("clipPath")
-      .attr("id", "clip-below")
-    .append("rect")
-      .attr("y", y(55))
-      .attr("width", width)
-      .attr("height", height - y(55));
+  svg.append("linearGradient")
+      .attr("id", "temperature-gradient")
+      .attr("gradientUnits", "userSpaceOnUse")
+      .attr("x1", 0).attr("y1", y(50))
+      .attr("x2", 0).attr("y2", y(60))
+    .selectAll("stop")
+      .data([
+        {offset: "0%", color: "steelblue"},
+        {offset: "50%", color: "gray"},
+        {offset: "100%", color: "red"}
+      ])
+    .enter().append("stop")
+      .attr("offset", function(d) { return d.offset; })
+      .attr("stop-color", function(d) { return d.color; });
 
   svg.append("g")
       .attr("class", "x axis")
@@ -73,11 +75,8 @@ function onRender() {
       .style("text-anchor", "end")
       .text("Temperature (ºF)");
 
-  svg.selectAll(".line")
-      .data(["above", "below"])
-    .enter().append("path")
-      .attr("class", function(d) { return "line " + d; })
-      .attr("clip-path", function(d) { return "url(#clip-" + d + ")"; })
+  svg.append("path")
       .datum(data)
+      .attr("class", "line")
       .attr("d", line);
 };
