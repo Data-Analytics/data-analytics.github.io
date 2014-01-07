442d2fdcecbf9d44cd35dd5c19851c513d96242f
Sushanth <sushanthece@gmail.com>
2014-01-07 17:04:39 +0530

    named the css with proper name

---
+++ sample.py
@@ -0,0 +1,13 @@
+import glob 
+import pandas
+data = glob.glob('*/')
+k = []
+for j in data:
+    j = data = glob.glob(j+'*.js')
+    for l in j:
+        k.append(l)
+        
+k = pandas.Series(k)
+list = k.unique()
+for y in list:
+    print y.split('\\')[1]
