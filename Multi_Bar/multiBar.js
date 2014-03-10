6e65659454447ffbd60e474ba950929bf4fd6054
Sushanth <sushanthece@gmail.com>
2014-03-10 18:59:13 +0530

    added multi bar, multi horizontal bar , cumulative chart

---
+++ Multi_Bar/multiBar.js
@@ -0,0 +1,36 @@
+nv.addGraph(function() {
+    var chart = nv.models.multiBarChart()
+      .transitionDuration(350)
+      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+      .rotateLabels(0)      //Angle to rotate x-axis labels.
+      .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+      .groupSpacing(0.1)    //Distance between each group of bars.
+    ;
+
+    chart.xAxis
+        .tickFormat(d3.format(',f'));
+
+    chart.yAxis
+        .tickFormat(d3.format(',.1f'));
+
+    d3.select('#chart')
+         .append('svg')
+        .attr('width','960px')
+        .attr('height','500px')
+        .datum(exampleData())
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+});
+
+//Generate some nice data.
+function exampleData() {
+  return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
+    return {
+      key: 'Stream #' + i,
+      values: data
+    };
+  });
+}
