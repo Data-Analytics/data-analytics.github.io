2a251685be4e1394b950c17ad0f377605e8e173e
Sushanth <sushanthece@gmail.com>
2014-03-10 19:18:58 +0530

    added 2 more charts

---
+++ Line_With_Focus/lineWithFocus.js
@@ -0,0 +1,36 @@
+nv.addGraph(function() {
+  var chart = nv.models.lineWithFocusChart();
+
+  chart.xAxis
+      .tickFormat(d3.format(',f'));
+
+  chart.yAxis
+      .tickFormat(d3.format(',.2f'));
+
+  chart.y2Axis
+      .tickFormat(d3.format(',.2f'));
+
+  d3.select('#chart')
+      .append('svg')
+      .attr('width','960px')
+      .attr('height','500px')
+      .datum(testData())
+      .transition().duration(500)
+      .call(chart);
+
+  nv.utils.windowResize(chart.update);
+
+  return chart;
+});
+/**************************************
+ * Simple test data generator
+ */
+
+function testData() {
+  return stream_layers(3,128,.1).map(function(data, i) {
+    return { 
+      key: 'Stream' + i,
+      values: data
+    };
+  });
+}
