fee6a468a776eb4a4170beb6bdeca3f10aafa913
sushanth <sushanthece@gmail.com>
2017-03-11 18:06:31 +0530

    intial outcome

--- Election_Data_2017/samp.py
+++ Election_Data_2017/samp.py
@@ -1,8 +1,7 @@
 import pandas as pd
-
-data = pd.read_csv('punjab.csv')
-data_latest = pd.read_csv('punjab_2017.csv')
-fixed_cols = data.columns
-data= pd.concat([data,data_latest],ignore_index=True)
-data.to_csv('punjab.csv',index=False)
-print (fixed_cols)
+for states in ['Punjab','Goa','Manipur','Uttar_Pradesh','Uttarakhand']:
+	data = pd.read_csv(states+'.csv')
+	data_latest = pd.read_csv(states+'_2017.csv')
+	fixed_cols = data.columns
+	data= pd.concat([data,data_latest],ignore_index=True)
+	data.to_csv(states+'.csv',index=False)
