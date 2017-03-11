de5f4920cd34f01a3e8592dc6d8f7a3c19fe1b11
sushanth <sushanthece@gmail.com>
2017-03-11 12:22:12 +0530

    intial commits

---
+++ Election_Data_2017/samp.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data = pd.read_csv('punjab.csv')
+data_latest = pd.read_csv('punjab_2017.csv')
+fixed_cols = data.columns
+data= pd.concat([data,data_latest],ignore_index=True)
+data.to_csv('punjab.csv',index=False)
+print (fixed_cols)
