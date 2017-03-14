8cbed8adc883eeaa8487b9d67b4e6a9fb4cfc6f3
sushanth <sushanthece@gmail.com>
2017-03-14 11:38:43 +0530

    updated the organised file names

--- Election_Data_2017/json_viewer.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import json
-json_data=open('uttarpradesh.assembly.json').read()
-data = json.loads(json_data)
-
-for value in data.values():
-	print(value)
--- Election_Data_2017/samp.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-for states in ['Punjab','Goa','Manipur','Uttar_Pradesh','Uttarakhand']:
-	data = pd.read_csv(states+'.csv')
-	data_latest = pd.read_csv(states+'_2017.csv')
-	fixed_cols = data.columns
-	data= pd.concat([data,data_latest],ignore_index=True)
-	data.to_csv(states+'.csv',index=False)
