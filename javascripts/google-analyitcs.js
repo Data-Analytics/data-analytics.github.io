c5d34327cc1b51dda44341c9a410a21057bb57e9
Sushanth <sushanthece@gmail.com>
2024-07-14 17:29:00 +0530

    Update google-analyitcs.js

--- javascripts/google-analyitcs.js
+++ javascripts/google-analyitcs.js
@@ -1,10 +1,19 @@
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
-  ga('require', 'displayfeatures');
-  ga('create', 'UA-45813188-1', 'data-analytics.github.io');
-  ga('require', 'linkid', 'linkid.js');
-  ga('send', 'pageview');
+$(document).ready( function(){
+  // Create the script element for Google Tag Manager
+  var gtmScript = document.createElement('script');
+  gtmScript.src = "https://www.googletagmanager.com/gtag/js?id=G-J2NT40QYX2";
+  gtmScript.async = true;
 
+  // Create the script element for configuring Google Analytics
+  var gaScript = document.createElement('script');
+  gaScript.textContent = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-J2NT40QYX2');
+  `;
+
+  // Append the scripts to the head of the document
+  document.head.appendChild(gtmScript);
+  document.head.appendChild(gaScript);
+});
